2748ef34c408bdaf8b7e95a1854813996f7b9c17
tienhv <tienhv@greenglobal.vn>
2022-09-26 12:36:40 +0700

    Code init code

---
+++ RabbitMq.service.Bus/RabbitMQ/IEventBusRabbitMQ.cs
@@ -0,0 +1,18 @@
+﻿using RabbitMq.Service.Bus.CommandBus;
+using RabbitMq.Service.Bus.Events.Core;
+
+namespace RabbitMq.Service.Bus.RabbitMQ
+{
+    public interface IEventBusRabbitMQ
+    {
+        void Publish(IntegrationEvent @event);
+
+        void Subscribe<T, TH>()
+            where T : IntegrationEvent
+            where TH : IIntegrationEventHandler<T>;
+
+        void Unsubscribe<T, TH>()
+            where TH : IIntegrationEventHandler<T>
+            where T : IntegrationEvent;
+    }
+}
